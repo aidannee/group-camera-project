4b986037430416a594dac57eec60893d2dff6254
aidannee <aidan.nee@students.beaminstitute.org>
2023-07-11 14:06:31 +0200

    test

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,13 +3,14 @@ import Webcam from "react-webcam";
 import DrawingCanvas from "./DrawingCanvas";
 const WebcamComponent = () => <Webcam />;
 const videoConstraints = {
-  width: 400,
-  height: 400,
+  width: 800,
+  height: 800,
   facingMode: "user",
 };
 function Profile() {
   const [picture, setPicture] = useState("");
   const webcamRef = React.useRef(null);
+  // galery of images
   let [savedGallery, setSavedGallery] = useState([]);
   let [imageIndex, setImageIndex] = useState(-1);
   let [isCanvasActive, setIsCanvasActive] = useState(false);
@@ -35,15 +36,14 @@ function Profile() {
     console.log(savedGallery[idx]);
     // console.log(savedGallery);
   }
-  function saveChanges(currentImage) {}
+  // function saveChanges(currentImage) {}
   return (
-    <div className="relative w-full h-full">
-      <h1 className="w-screen z-50 text-center bg-blue-500 text-white text-5xl p-2">
-        Image Capturing
+    <div className="h-screen overflow-hidden">
+      <h1 className="bg-gray-200  text-center font-bold text-xl p-2 m-2">
+        Capturing Images
       </h1>
-      <div className="flex  h-full w-full">
-        {/* Gallery area */}
-        <div className="w-60 h-screen flex flex-col bg-red-500 gap-4 p-4 overflow-y-auto top-16">
+      <div className="flex ">
+        <div className="w-1/4 h-screen overflow-y-auto  bg-red-500 gap-4 p-4   ">
           {savedGallery.map((img, index) => {
             return (
               <div key={index}>
@@ -54,92 +54,92 @@ function Profile() {
                   src={img}
                   alt="Image"
                 />
-                <div className=" mt-2 flex flex-col items-center">
-                  <div className="flex ">
-                    {/* edit */}
-                    <button
-                      className=" bg-yellow-400 rounded p-2"
-                      onClick={(e) => drawImage(img, index)}
+                <div className="flex items-center">
+                  <div className="flex">
+                    <a
+                      className="flex-1"
+                      href={savedGallery[index]}
+                      download={index}
                     >
-                      &#10000;
-                    </button>
-
-                    {/* delete */}
+                      <button className="  bg-green-400 rounded  p-2">
+                        &#8681;
+                      </button>
+                    </a>
                     <button
                       className=" bg-pink-400 mx-2 rounded p-2"
                       onClick={(e) => deleteImage(index)}
                     >
                       &#128465;
                     </button>
-                    {/* download */}
-                    <a className="" href={savedGallery[index]} download={index}>
-                      <button className="  bg-green-400 rounded  p-2">
-                        &#8681;
-                      </button>
-                    </a>
+                    <button
+                      className=" bg-yellow-400 rounded p-2"
+                      onClick={(e) => drawImage(img, index)}
+                    >
+                      &#10000;
+                    </button>
                   </div>
                 </div>
               </div>
             );
           })}
         </div>
-        {/* Right side */}
-        <div className="absolute top-16 left-60 w-screen bg-green-500">
-          <div>
-            {picture === "" ? (
-              <Webcam
-                className="h-96 p-4"
-                audio={false}
-                ref={webcamRef}
-                screenshotFormat="image/jpeg"
-                videoConstraints={videoConstraints}
-              />
-            ) : (
-              <div className="w-2/3 p-4"></div>
-            )}
-            <div>
-              {picture !== "" ? (
-                <div className="w-2/3 p-4">
-                  <button
-                    onClick={handleRetake}
-                    className="bg-blue-400 px-4 py-2 m-4 "
-                  >
-                    Retake
-                  </button>
-                </div>
-              ) : (
-                <button onClick={capture} className="bg-red-400 px-4 py-2 m-4 ">
-                  Capture
-                </button>
-              )}
-            </div>
-          </div>
-          {/* Canvas area */}
-          <div className="w-full z-50">
+        <div className="h-screen w-3/4 bg-green-500  overflow-auto ">
+          <div className="   w-full z-50">
             {isCanvasActive ? (
-              <div>
+              <>
                 <DrawingCanvas
                   image={savedGallery[imageIndex]}
                   setSavedGallery={setSavedGallery}
                   savedGallery={savedGallery}
                   imgIdx={imageIndex}
                 />
-                {/* <button
-                  onClick={() => saveChanges(savedGallery[imageIndex])}
-                  className="bg-blue-400 px-4 py-2 m-4 "
-                >
-                  Save Changes
-                </button> */}
-              </div>
+              </>
             ) : (
-              ""
+              <div>
+                {picture === "" ? (
+                  <Webcam
+                    className="h-96 p-4"
+                    audio={false}
+                    ref={webcamRef}
+                    screenshotFormat="image/png"
+                    // screenshotFormat="image/jpeg"
+                    videoConstraints={videoConstraints}
+                  />
+                ) : (
+                  ""
+                )}
+                {/* ( <div className="w-2/3 p-4"></div>) */}
+                <div>
+                  {picture !== "" ? (
+                    <div className="w-2/3 p-4">
+                      {/* <button
+                        onClick={handleRetake}
+                        className="bg-blue-400 px-4 py-2 m-4 "
+                      >
+                        Retake
+                      </button> */}
+                    </div>
+                  ) : (
+                    <button
+                      onClick={capture}
+                      className="bg-red-400 px-4 py-2 m-4 "
+                    >
+                      Capture
+                    </button>
+                  )}
+                </div>
+              </div>
             )}
+            <button
+              onClick={handleRetake}
+              className="bg-blue-400 px-4 py-2 m-4 "
+            >
+              Retake
+            </button>
             {/* <img src={savedGallery[imageIndex]} alt="" /> */}
           </div>
-          {/* Camera input */}
         </div>
       </div>
-      {/* <div className="bg-slate-500 opacity-80 top-28 left-52  w-full h-full absolute"></div> */}
     </div>
   );
 }
