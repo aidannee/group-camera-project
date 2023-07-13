b3af6473f9d7dac3d1791c0dc550983e9a6c2e6f
aidannee <aidan.nee@students.beaminstitute.org>
2023-07-13 09:53:32 +0200

    test

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -26,6 +26,7 @@ function Profile() {
   }, [savedGallery]);
   const handleRetake = () => {
     setPicture(""); // Reset the picture state to an empty string
+    setIsCanvasActive(false);
   };
   function deleteImage(index) {
     setSavedGallery((preImg) => preImg.filter((img, idx) => idx !== index));
@@ -38,109 +39,103 @@ function Profile() {
   }
   // function saveChanges(currentImage) {}
   return (
-    <div className="h-screen overflow-hidden">
-      <h1 className="bg-gray-200  text-center font-bold text-xl p-2 m-2">
-        Capturing Images
-      </h1>
-      <div className="flex ">
-        <div className="w-1/4 h-screen overflow-y-auto  bg-red-500 gap-4 p-4   ">
-          {savedGallery.map((img, index) => {
-            return (
-              <div key={index}>
-                <img
-                  onClick={(e) => {
-                    setImageIndex(index);
-                  }}
-                  src={img}
-                  alt="Image"
-                />
-                <div className="flex items-center">
-                  <div className="flex">
-                    <a
-                      className="flex-1"
-                      href={savedGallery[index]}
-                      download={index}
-                    >
-                      <button className="  bg-green-400 rounded  p-2">
-                        &#8681;
+    <>
+      <div className=" overflow-hidden h-screen flex flex-col ">
+        <h1 className="bg-gray-200 text-center font-bold text-xl p-2 m-2">
+          Hello World
+        </h1>
+        <div className="flex  h-[calc(100vh-60px)]">
+          {/* Left part content */}
+          <div className="w-1/5 bg-red-500 p-4  overflow-y-auto  h-full">
+            {/* Place your content here */}
+            {savedGallery.map((img, index) => {
+              return (
+                <div key={index}>
+                  <img
+                    onClick={(e) => {
+                      setImageIndex(index);
+                    }}
+                    src={img}
+                    alt="Image"
+                  />
+                  <div className="flex flex-col">
+                    <div className="flex ">
+                      <button
+                        className="flex-1  bg-yellow-400 my-2 p-2"
+                        onClick={(e) => deleteImage(index)}
+                      >
+                        Delete
                       </button>
-                    </a>
+                      <a
+                        className="flex-1"
+                        href={savedGallery[index]}
+                        download={index}
+                      >
+                        <button className="  bg-green-400 m-2 p-2">
+                          Download
+                        </button>
+                      </a>
+                    </div>
                     <button
-                      className=" bg-pink-400 mx-2 rounded p-2"
-                      onClick={(e) => deleteImage(index)}
-                    >
-                      &#128465;
-                    </button>
-                    <button
-                      className=" bg-yellow-400 rounded p-2"
+                      className=" bg-yellow-400 my-2 p-2"
                       onClick={(e) => drawImage(img, index)}
                     >
-                      &#10000;
+                      Draw
                     </button>
                   </div>
                 </div>
-              </div>
-            );
-          })}
-        </div>
-        <div className="h-screen w-3/4 bg-green-500  overflow-auto ">
-          <div className="   w-full z-50">
-            {isCanvasActive ? (
-              <>
-                <DrawingCanvas
-                  image={savedGallery[imageIndex]}
-                  setSavedGallery={setSavedGallery}
-                  savedGallery={savedGallery}
-                  imgIdx={imageIndex}
+              );
+            })}
+          </div>
+          {/* Right part content */}
+          <div className=" w-4/5 bg-green-500 p-4 overflow-y-auto  ">
+            <div className="flex flex-col ">
+              {/* Right part content */}
+              {/* Place your content here */}
+              {picture === "" ? (
+                <Webcam
+                  audio={false}
+                  ref={webcamRef}
+                  screenshotFormat="image/png"
+                  videoConstraints={videoConstraints}
                 />
-              </>
-            ) : (
+              ) : (
+                ""
+              )}
               <div>
-                {picture === "" ? (
-                  <Webcam
-                    className="h-96 p-4"
-                    audio={false}
-                    ref={webcamRef}
-                    screenshotFormat="image/png"
-                    // screenshotFormat="image/jpeg"
-                    videoConstraints={videoConstraints}
+                {picture !== "" ? (
+                  <button
+                    onClick={handleRetake}
+                    className="bg-blue-400 px-4 py-2 m-4 "
+                  >
+                    Retake
+                  </button>
+                ) : (
+                  <button
+                    onClick={capture}
+                    className="bg-red-400 px-4 py-2 m-4 "
+                  >
+                    Capture
+                  </button>
+                )}
+              </div>
+              <div className=" z-20">
+                {isCanvasActive ? (
+                  <DrawingCanvas
+                    image={savedGallery[imageIndex]}
+                    setSavedGallery={setSavedGallery}
+                    savedGallery={savedGallery}
+                    imgIdx={imageIndex}
                   />
                 ) : (
                   ""
                 )}
-                {/* ( <div className="w-2/3 p-4"></div>) */}
-                <div>
-                  {picture !== "" ? (
-                    <div className="w-2/3 p-4">
-                      {/* <button
-                        onClick={handleRetake}
-                        className="bg-blue-400 px-4 py-2 m-4 "
-                      >
-                        Retake
-                      </button> */}
-                    </div>
-                  ) : (
-                    <button
-                      onClick={capture}
-                      className="bg-red-400 px-4 py-2 m-4 "
-                    >
-                      Capture
-                    </button>
-                  )}
-                </div>
               </div>
-            )}
-            <button
-              onClick={handleRetake}
-              className="bg-blue-400 px-4 py-2 m-4 "
-            >
-              Retake
-            </button>
-            {/* <img src={savedGallery[imageIndex]} alt="" /> */}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 export default Profile;
